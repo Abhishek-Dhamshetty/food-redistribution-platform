f04a63e3546aa31a8279b75b9fddc4a6e4bd9d06
Abhishek-Dhamshetty <abhishekdhamshetty@gmail.com>
2024-11-24 22:58:46 +0530

    Your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import "bootstrap/dist/css/bootstrap.min.css";`,
-      },
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   },
-});
+  server: {
+    port: 3000
+  }
+})
